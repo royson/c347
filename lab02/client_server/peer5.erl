91ea2181b4df4cd09881d2f934dbe41563db015e
Royson Lee <dsl114@cloud-vm-45-211>
2017-02-01 22:11:51 +0000

    Recode version 4 to accommodate version 5.

---
+++ lab02/client_server/peer5.erl
@@ -0,0 +1,48 @@
+
+%%% distributed algorithms, n.dulay, 4 jan 17
+%%% simple client-server, v1
+
+-module(peer5).
+-export([start/0]).
+ 
+start() ->
+  receive
+    {bind, Neighbours} ->
+    next(Neighbours, 0, null, 0)
+  end.
+ 
+next(Neighbours, MCount, Parent, Children) ->
+  receive 
+    {hello, Dist, Source} ->
+      if MCount == 0 ->
+%        io:format("Message is ~p for peer ~p, Dist: ~p~n", [hello, self(), Dist]),
+        timer:sleep(1000),
+        [Neighbour ! {hello, Dist+1, self()} || Neighbour <- Neighbours, Neighbour /= Source],
+	if Source /= null ->
+		Source ! {children};
+		true -> ok
+	end,
+	F = 1;
+	true ->
+	F = 2
+      end;
+    {children} ->
+	F = 3,
+	%dummy value%
+	Source = Parent
+  end,
+	%F = 1 -> (first hello packet received)
+	%F = 2 -> (more than 1 hello packet received)
+	%F = 3 -> (children packet received)
+  if 
+	F == 1 -> 
+		io:format("Peer ~p Parent ~p Children ~p~n", [self(), Source, Children]), 
+        	next(Neighbours, MCount+1, Source, Children);
+        F == 2 -> 
+		next(Neighbours, MCount+1, Parent, Children);
+	F == 3 ->
+		io:format("Peer ~p Parent ~p Children ~p~n", [self(), Parent, Children+1]), 
+		next(Neighbours, MCount, Parent, Children+1);
+	true -> ok
+  end.
+        
---
+++ lab02/client_server/system5.erl
@@ -0,0 +1,36 @@
+
+%%% distributed algorithms, n.dulay, 4 jan 17
+%%% simple client-server, v1
+
+%%% run all processes on one node
+ 
+-module(system5).
+-export([start/1]).
+
+start([N|_]) ->
+  {A, _} = string:to_integer(atom_to_list(N)),
+  List = lists:seq(1,A),
+  Peers = [ spawn(peer5, start, []) || _ <- List],
+
+  neighbours(Peers, 1, [2, 7]),
+  neighbours(Peers, 2, [1, 3, 4]),
+  neighbours(Peers, 3, [2, 4, 5]),
+  neighbours(Peers, 4, [2, 3, 6]),
+  neighbours(Peers, 5, [3]),
+  neighbours(Peers, 6, [4]),
+  neighbours(Peers, 7, [1, 8]),
+  neighbours(Peers, 8, [7, 9, 10]),
+  neighbours(Peers, 9, [8, 10]),
+  neighbours(Peers, 10, [8, 9]),
+
+  FirstPeer = lists:nth(5,Peers),
+  FirstPeer ! {hello, 0, self()},
+
+  receive
+    {Result} ->
+	io:format("Result: ~p~n", [Result])
+  end.
+
+neighbours(Peers, X, Neighbours) ->
+  NeighboursPID = [ lists:nth(Neighbour,Peers) || Neighbour <- Neighbours ],
+  lists:nth(X, Peers) ! {bind, NeighboursPID}.
