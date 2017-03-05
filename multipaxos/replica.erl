71593c12bf7995d2445315668ecf376f8781cc0a
Lee De Sheng Royson <de.lee14@imperial.ac.uk>
2017-03-05 14:28:24 +0000

    Convert list to set for replica

--- multipaxos/replica.erl
+++ multipaxos/replica.erl
@@ -8,29 +8,42 @@
 start(Database) ->
   receive
     {bind, Leaders} -> 
-       next(Leaders, 1, 1, 1, [], [], [], Database)
+       next(Leaders, 1, 1, 1, sets:new(), sets:new(), sets:new(), Database)
   end.
 
 next(Leaders, State, SlotIn, SlotOut, Reqs, Proposals, Decisions, Database) ->
   receive
     {request, C} ->      % request from client
-      Reqs2 = Reqs ++ [C],
-      propose(Leaders, State, SlotIn, SlotOut, Reqs2, Proposals, Decisions, Database);
+      %Reqs2 = Reqs ++ [C],
+      Reqs2 = sets:add_element(C, Reqs),
+      Decisions2 = sets:to_list(Decisions),
+      ReqsList = sets:to_list(Reqs2),
+      ProposalsList = sets:to_list(Proposals),
+      propose(Leaders, State, SlotIn, SlotOut, ReqsList, ProposalsList, Decisions2, Database);
     {decision, S, C} ->  % decision from commander
-      Decisions2 = Decisions ++ [{S, C}],
-      {Proposals2, Reqs2, SlotOut2, State2} = decide (Decisions2, Proposals, Reqs, SlotOut, State, Decisions2, Database),
+      DecisionsSet = sets:add_element({S,C}, Decisions),
+      Decisions2 = sets:to_list(DecisionsSet),
+      ReqsList = sets:to_list(Reqs),
+      ProposalsList = sets:to_list(Proposals),
+      {Proposals2, Reqs2, SlotOut2, State2} = decide (Decisions2, ProposalsList, ReqsList, SlotOut, State, Decisions2, Database),
       propose(Leaders, State2, SlotIn, SlotOut2, Reqs2, Proposals2, Decisions2, Database)
   end.
 
 propose(Leaders, State, SlotIn, SlotOut, [], Proposals, Decisions, Database) ->
-  next(Leaders, State, SlotIn, SlotOut, [], Proposals, Decisions, Database);
+  DecisionsSet = sets:from_list(Decisions),
+  ProposalsSet = sets:from_list(Proposals),
+  ReqsSet = sets:from_list([]),
+  next(Leaders, State, SlotIn, SlotOut, ReqsSet, ProposalsSet, DecisionsSet, Database);
 propose(Leaders, State, SlotIn, SlotOut, [C | T], Proposals, Decisions, Database) ->
   WINDOW = 5,
   if SlotIn < SlotOut + WINDOW ->
     case isMemberOf(SlotIn, Decisions) of
     %case lists:member(SlotIn, [ SO || {SO, _} <- Decisions ]) of
       false ->
-        Proposals2 = Proposals ++ [{SlotIn, C}],
+        %Proposals2 = Proposals ++ [{SlotIn, C}],
+        ProposalsSet = sets:from_list(Proposals),
+        ProposalsT = sets:add_element({SlotIn, C}, ProposalsSet),
+        Proposals2 = sets:to_list(ProposalsT),
         [ L ! {propose, SlotIn, C} || L <- Leaders ],
         SlotIn2 = SlotIn + 1,
         propose(Leaders, State, SlotIn2, SlotOut, T, Proposals2, Decisions, Database);
@@ -39,7 +52,10 @@ propose(Leaders, State, SlotIn, SlotOut, [C | T], Proposals, Decisions, Database
         propose(Leaders, State, SlotIn2, SlotOut, [C | T], Proposals, Decisions, Database)
     end;
   true -> 
-    next(Leaders, State, SlotIn, SlotOut, [C | T], Proposals, Decisions, Database)
+    DecisionsSet = sets:from_list(Decisions),
+    ProposalsSet = sets:from_list(Proposals),
+    ReqsSet = sets:from_list([C | T]),
+    next(Leaders, State, SlotIn, SlotOut, ReqsSet, ProposalsSet, DecisionsSet, Database)
   end.
   
    
@@ -47,12 +63,17 @@ decide([], Proposals, Reqs, SlotOut, State, _, _) ->
   {Proposals, Reqs, SlotOut, State};
 decide([{SO, C2} | T], Proposals, Reqs, SlotOut, State, Decisions, Database) ->
   case isMemberOf(SO, Proposals) of
-  %case lists:member(SO, [ SO2 || {SO2 , _} <- Proposals ]) of
     true ->
       {SO2, C3} = lists:last([ {_SO2, _C3} || {_SO2, _C3} <- Proposals, SO == _SO2 ]),
-      Proposals2 = Proposals -- [{SO2, C3}],
+      %Proposals2 = Proposals -- [{SO2, C3}],
+      ProposalsSet = sets:from_list(Proposals),
+      ProposalsT = sets:del_element({SO2, C3}, ProposalsSet),
+      Proposals2 = sets:to_list(ProposalsT),
       if C2 /= C3 ->
-        Reqs2 = Reqs ++ [C3],
+        %Reqs2 = Reqs ++ [C3],
+        ReqsSet = sets:from_list(Reqs),
+        ReqsT = sets:add_element(C3, ReqsSet),
+        Reqs2 = sets:to_list(ReqsT),
         perform(C2, T, Proposals2, Reqs2, SlotOut, State, Decisions, Database);
       true ->
         perform(C2, T, Proposals2, Reqs, SlotOut, State, Decisions, Database)
